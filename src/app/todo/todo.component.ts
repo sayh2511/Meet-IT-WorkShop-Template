47daf884040ca67fa3913b437795750b658e3817
Sinan Ayhan <sinan.ayhan@kaufland.com>
2019-10-18 14:20:32 +0200

    Template

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,30 +7,6 @@ import { Component } from '@angular/core';
 })
 export class TodoComponent {
 
-  todoInput: string;
-  todos: Array<string> = ['Theoretische Informatik lernen!!'];
-  removableIndex: number;
+  constructor() { }
 
-  constructor(private http: HttpClient) { }
-
-  addNewTodo() {
-    if (this.todoInput.length > 0) {
-      this.todos.push(this.todoInput);
-      this.todoInput = '';
-    }
-  }
-
-  removeIndex() {
-    const index = +this.removableIndex;
-    if (index) {
-      this.todos.splice(index - 1, 1);
-      delete this.removableIndex;
-    }
-  }
-
-/*  getTodos(): void {
-    this.http.get<any>('https://meet-it.apps.eu01.cloud.schwarz/todos').subscribe(data => {
-      this.todos = data;
-    });
-  }*/
 }
